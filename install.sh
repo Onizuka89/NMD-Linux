eb364040e2bcad6da00641397f4b2cda81450622
Stian Drøbak <sum41rules4@hotmail.com>
2013-07-04 19:56:54 +0200

    bug-fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-if [ -f /usr/share/X11/xkb/symbols/no && -f /usr/share/X11/xkb/rules/evdev.xml  ]; then
+if [[ -f /usr/share/X11/xkb/symbols/no && -f /usr/share/X11/xkb/rules/evdev.xml  ]]; then
     sudo cat ./no >> /usr/share/X11/xkb/symbols/no
 
     echo "Currently no support for adding auto adding to evdev.xml.
