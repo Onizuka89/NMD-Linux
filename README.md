1df7b47b141874db76d181f8e58b4a5cceae3b51
Stian Drøbak <sum41rules4@hotmail.com>
2013-07-04 19:51:48 +0200

    added a early installer

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ NMD-Linux
 =========
 
 Norsk Mac Dvorak for Linux.
+
+Det er ingen garanti for at installeren vil kjøre som antatt, så bruk på egen risiko.
+Installeren vil kun oppdatere no symbol filen for øyeblikket.
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+if [ -f /usr/share/X11/xkb/symbols/no && -f /usr/share/X11/xkb/rules/evdev.xml  ]; then
+    sudo cat ./no >> /usr/share/X11/xkb/symbols/no
+
+    echo "Currently no support for adding auto adding to evdev.xml.
+Add following variant to Norwegian in the evdev.xml file:"
+    echo '<variant>
+    <configItem>
+        <name>mac_dvorak</name>
+        <description>Norwegian Dvorak(Macintosh)</description>
+    </configItem>
+</variant>'
+
+fi
